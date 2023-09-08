1609ddbde4ec8c69ba9c003cc9e7f35cd5a193da
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-07 18:35:57 -0600

    add a component to list jefes

---
+++ src/component/listJefes.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function ListJefes(){
+    return(
+        <>
+        <div>
+            <p>Ingenieria en sistemas</p>
+            <p>Nancy Ruiz</p>
+            <a>Editar</a>
+        </div>
+        <div>
+            <p>Ingenieria en sistemas</p>
+            <p>Nancy Ruiz</p>
+            <a>Editar</a>
+        </div>
+        <div>
+            <p>Ingenieria en sistemas</p>
+            <p>Nancy Ruiz</p>
+            <a>Editar</a>
+        </div>
+        <div>
+            <p>Ingenieria en sistemas</p>
+            <p>Nancy Ruiz</p>
+            <a>Editar</a>
+        </div>
+        </>
+    );
+}
+
+export default ListJefes;
