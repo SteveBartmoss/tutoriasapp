c33cb672d1a6246f824a65bdbd18da7ff11606d6
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-07 18:15:53 -0600

    add a login page for the proyect

---
+++ src/pages/login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import OnLogin from "../component/onLogin";
+
+function Login() {
+    return (
+        <>
+            <div>
+                <h1>Login</h1>
+                <OnLogin />
+            </div>
+        </>
+    );
+}
+
+export default Login;
