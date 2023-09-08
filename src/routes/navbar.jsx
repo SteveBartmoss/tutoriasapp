a362d145570b5493fd1b40cda71488eb9f4b31c1
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-07 18:45:32 -0600

    add a new routes in the router

---
+++ src/routes/navbar.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NavBar(){
+    return(
+        <div>
+            <ul>
+                <p>Amdin</p>
+                <li>
+                    <Link to="/admuser">Creacion de usuarios</Link>
+                </li>
+                <li>
+                    <Link to="/kardex">Captura de Kardex</Link>
+                </li>
+                <li>
+                    <Link to="/consultas">Consultas</Link>
+                </li>
+            </ul>
+
+            <ul>
+                <p>Academia</p>
+                <li>
+                    <Link to="/jefes">Editar Jefes de Carrera</Link>
+                </li>
+                <li>
+                    <Link>Seleccion de Calendario</Link>
+                </li>
+                <li>
+                    <Link>Plan de estudios</Link>
+                </li>
+            </ul>
+
+            <ul>
+                <p>Tutores</p>
+                <li>
+                    <Link>Agregar y Editar Tutores</Link>
+                </li>
+
+                <li>
+                    <Link>Asignar Materias</Link>
+                </li>
+
+                <li>
+                    <Link>Datos Personales</Link>
+                </li>
+            </ul>
+
+            <ul>
+                <p>Alumnos</p>
+                <li>
+                    <Link>Agregar y Editar Alumnos</Link>
+                </li>
+
+                <li>
+                    <Link>Ficha Medica y Psicologica</Link>
+                </li>
+
+                <li>
+                    <Link>Datos Personales</Link>
+                </li>
+            </ul>
+
+            <ul>
+                <p>Grupos</p>
+
+                <li>
+                    <Link>Agregar y Editar Grupos</Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default NavBar;
