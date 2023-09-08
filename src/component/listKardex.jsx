ce78d5b9878bbe379405e854d408a9cb812c7d11
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-07 18:37:20 -0600

    add a component to list kardex

---
+++ src/component/listKardex.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function ListKardex(){
+    return(
+        <>
+        <div>
+            <div>
+                <h1>Busqueda y seleccion manual</h1>
+                <div>
+                    <p>Grupo A tutor 1</p>
+                    <a>Editar</a>
+                </div>
+                <div>
+                    <p>Grupo B tutor 1</p>
+                    <a>Editar</a>
+                </div>
+                <div>
+                    <p>Grupo C tutor 1</p>
+                    <a>Editar</a>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default ListKardex;
