1dc036ccf81016ce6820da0e983ccf3a130bb47c
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-07 18:35:25 -0600

    add a component to list calificaciones

---
+++ src/component/listCalificacion.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function ListCalificiacion(){
+    return(
+        <>
+        <div>
+            <div>
+                <p>Claificion 1</p>
+                <input type="text" />
+            </div>
+            <div>
+                <p>Claificion 2</p>
+                <input type="text" />
+            </div>
+            <div>
+                <p>Claificion 3</p>
+                <input type="text" />
+            </div>
+            <div>
+                <p>Claificion 4</p>
+                <input type="text" />
+            </div>
+            <button>Guardar</button>
+        </div>
+        </>
+    );
+}
+
+export default ListCalificiacion;
