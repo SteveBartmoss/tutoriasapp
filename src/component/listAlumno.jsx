3abf5670fd8984c9b0ab6d2421a625ac44fc4520
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-07 18:34:36 -0600

    add a component to list a alumnos

---
+++ src/component/listAlumno.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function ListAlumno(){
+    return(
+        <>
+        <div>
+            <h1>Grupo A tutor 1</h1>
+            <div>
+                <p>Steve</p>
+                <p>19012397</p>
+                <a>Editar</a>
+            </div>
+            <div>
+                <p>Marco</p>
+                <p>19012397</p>
+                <a>Editar</a>
+            </div>
+            <div>
+                <p>Kevin</p>
+                <p>19012397</p>
+                <a>Editar</a>
+            </div>
+            <div>
+                <p>Enrique</p>
+                <p>19012397</p>
+                <a>Editar</a>
+            </div>
+        </div>
+        </>
+    );
+}
