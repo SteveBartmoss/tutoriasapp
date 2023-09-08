f89ea0340564ad08a453c6d3b5ab02a0f7fd7eb9
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-07 18:38:48 -0600

    add a page to module jefes de carreras

---
+++ src/pages/JefesCarrera.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ListJefes from "../component/listJefes";
+
+function JefesCarrera(){
+    return(
+        <>
+        <div>
+            <h1>Jefes de carrera</h1>
+        </div>
+        <ListJefes /> 
+        </>
+    );
+}
+
+export default JefesCarrera;
