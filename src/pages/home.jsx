ebb3ae5a21986fa2c2350665f1b18b0887336bf4
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-07 18:39:41 -0600

    add a home page

---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../routes/navbar";
+
+function Home(){
+    return(
+        <>
+        <div>
+            <NavBar/>
+            <h1>Home page</h1>
+        </div>
+        </>
+    );
+}
+
+export default Home;
