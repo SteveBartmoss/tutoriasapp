0ef4f0ebe77ad194f1dc91a488a699937a482644
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-15 16:54:29 -0600

    add a functions to state of the fields in the form user, add a function for send a user to the database with the api, modify the form user to use the state an call the api

--- src/pages/amduser.jsx
+++ src/pages/amduser.jsx
@@ -1,22 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import { createUser } from "../helpers/userApi";
 
 function Admuser(){
+
+    const [user,setUser]=useState('');
+    const [name,setName]=useState('');
+    const [pass,setPass]=useState('');
+    const [type,setType]=useState(0);
+    const [recover,setRecover]=useState('');
+
+    const onChangeUser=(event)=>{
+        setUser(event.target.value);
+    }
+
+    const onChangeName=(event)=>{
+        setName(event.target.value);
+    }
+    
+    const onChangePass=(event)=>{
+        setPass(event.target.value);
+    }
+
+    const onChangeType=(event)=>{
+        setType(event.target.value);
+    }
+
+    const onChangeRecover=(event)=>{
+        setRecover(event.target.value);
+    }
+
+    const onSubmitUser= async (userp,name,pass,type,recover)=>{
+        let user={
+            user: userp,
+            nombre: name,
+            pass: pass,
+            tipo: 1,
+            recover: recover
+        }
+        console.log(user);
+        const result = await createUser(user);
+
+        console.log(result);
+    }
     return(
         <>
         <h1>Admin Usuarios</h1>
-        <p>Nombres</p>
-        <input type="text" placeholder="steve"/>
-        <p>Apellido Paterno</p>
-        <input type="text" placeholder="jobs"/>
-        <p>Apellido Materno</p>
-        <input type="text" placeholder="gate"/>
-        <p>Correo electronico</p>
-        <input type="text" placeholder="gate"/>
+        <p>Usuario</p>
+        <input type="text" value={user} placeholder="gate"
+        onChange={(event)=>onChangeUser(event)}/>
+        <p>Nombre</p>
+        <input type="text" value={name} placeholder="gate"
+        onChange={(event)=>onChangeName(event)}/>
         <p>Password</p>
-        <input type="password" placeholder="pasword"/>
+        <input type="password" value={pass} placeholder="pasword"
+        onChange={(event)=>onChangePass(event)}/>
         <p>Rol</p>
-        <input type="text" placeholder="admin"/>
-        <button type="button">Guardar</button>
+        <input type="text" value={type} placeholder="admin"
+        onChange={(event)=>onChangeType(event)}/>
+        <p>Recover</p>
+        <input type="text" value={recover} placeholder="admin"
+        onChange={(event)=>onChangeRecover(event)}/>
+        <button type="button" onClick={()=>onSubmitUser(user,name,pass,type,recover)} >Guardar</button>
         </>
     );
 }
