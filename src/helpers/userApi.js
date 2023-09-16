a0b9d981b8b8364723af827dd7fa57549cbb1b19
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-16 17:49:05 -0600

    remove a code was not necesary now

--- src/helpers/userApi.js
+++ src/helpers/userApi.js
@@ -8,16 +8,3 @@ export const createUser= async (user)=>{
 export const getUsers= async ()=>{
     return await axios.get('http://localhost:3300/api/users/')
 }
-
-export const createUser2=async ({usuario,name,pass,tipo,recover})=>{
-    return await axios({
-        method: 'post',
-        url: 'http://localhost:3300/api/users',
-        data: {
-            user: usuario,
-            nombre: name,
-            pass: pass,
-            
-        }
-    });
-}
