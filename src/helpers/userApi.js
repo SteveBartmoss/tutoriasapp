86c77ae386c241b2468cb4329ab33cff0622799b
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-16 12:35:48 -0600

    add a function to get all users

--- src/helpers/userApi.js
+++ src/helpers/userApi.js
@@ -5,6 +5,10 @@ export const createUser= async (user)=>{
     return await axios.post('http://localhost:3300/api/users',user);
 }
 
+export const getUsers= async ()=>{
+    return await axios.get('http://localhost:3300/api/users/')
+}
+
 export const createUser2=async ({usuario,name,pass,tipo,recover})=>{
     return await axios({
         method: 'post',
