4cbe7197aac7808d2769f149d79e291b1ebf1d2c
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-16 18:00:40 -0600

    add a function to delete user by username

--- src/helpers/userApi.js
+++ src/helpers/userApi.js
@@ -6,5 +6,9 @@ export const createUser= async (user)=>{
 }
 
 export const getUsers= async ()=>{
-    return await axios.get('http://localhost:3300/api/users/')
+    return await axios.get('http://localhost:3300/api/users/');
+}
+
+export const deleteUser= async (id)=>{
+    return await axios.delete(`http://localhost:3300/api/users/${id}`);
 }
